d4aae1c97071fe0a50581bf843bc9740e200cdb1
neeljoshii <73433085+neeljoshii@users.noreply.github.com>
2025-02-27 19:21:05 +0530

    models.py

--- models.py
+++ models.py
@@ -8,4 +8,6 @@ class Products(db.Model):
     __tablename__ = 'Products'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+    description = db.Column(db.Text)
     price = Column(db.Float)
+    created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
