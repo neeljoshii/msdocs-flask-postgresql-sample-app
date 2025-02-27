2fbe2880eb11625d14657399ef2d23d85aae0e17
neeljoshii <73433085+neeljoshii@users.noreply.github.com>
2025-02-27 14:51:46 +0530

    models.py

--- models.py
+++ models.py
@@ -4,29 +4,11 @@ from sqlalchemy.orm import validates
 from app import db
 
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
+class Product(db.Model):
+    __tablename__ = 'Products'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
+    price = Column(Float))
 
     def __str__(self):
         return self.name
-
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
-
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
