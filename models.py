4beb34b41bdb124269ef4e22bb9c4829a5d21cad
neeljoshii <73433085+neeljoshii@users.noreply.github.com>
2025-02-27 15:06:47 +0530

    models.py

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Product(db.Model):
     __tablename__ = 'Products'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    price = Column(Float))
+    price = Column(Float)
 
     def __str__(self):
         return self.name
